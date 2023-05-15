3fd1e48a0375542ea21c0298529e0e0809290b86
Kevikajyothi <jyothikevika2@gmail.com>
2023-05-15 11:21:02 +0530

    js

---
+++ one.js
@@ -0,0 +1,56 @@
+var p_name="Nike"
+var avail="N"
+
+var discount1=true
+var discount2=false
+
+var p_views= 350
+
+var rating=3.9;
+
+let specialization={
+    fitness:"slim",
+    size:"xl",
+    colour:"red"
+
+}
+
+var size=[40,42,44,46]
+
+var colour=["blue","white","black","red"]
+
+var p_delivery=[3,4 ]
+
+var name=["kevika","gayathri","tinky","pinky"]
+
+
+console.log(p_name);
+console.log(typeof(p_name));
+
+console.log(discount1);
+console.log(typeof(discount1));
+
+console.log(discount2);
+console.log(typeof(discount2));
+
+console.log(rating);
+console.log(typeof(rating));
+
+console.log(p_views);
+console.log(typeof(p_views));
+
+console.log(specialization);
+console.log(typeof(specialization));
+
+console.log(size);
+console.log(typeof(size));
+
+console.log(colour);
+console.log(typeof(colour));
+
+console.log(p_delivery );
+console.log(typeof(p_delivery ));
+
+console.log(name);
+console.log(typeof(name));
+
