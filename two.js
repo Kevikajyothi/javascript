92aaae183af79bcc8174f717317293a46eb51238
Kevikajyothi <jyothikevika2@gmail.com>
2023-05-17 15:28:59 +0530

    for loop

---
+++ two.js
@@ -0,0 +1,68 @@
+for(let i=1;i<=10;i++){
+    console.log(i);
+}
+
+for(let i=5;i<=15;i++){
+    console.log(i);
+}
+
+for(let i=15;i>=10;i--){
+    console.log(i);
+}
+
+for (let i=0;i<=10;i=i+2){
+    console.log(i);
+}
+
+for (let i=1;i<=10;i=i+2){
+    console.log(i);
+}
+
+for (let i=10;i>=1;i--){
+    console.log(i);
+}
+
+for (let i=1;i<=10;i++){
+    console.log("4*",i,"=",4*i);
+}
+
+
+for(let i=1;i<=10;i++){
+    console.log("6*",i,"=",6*i);
+}
+
+for(let i=10;i>=1;i--){
+    console.log("8*",i,"=",8*i);
+}
+
+initialization = i=1;
+condition      = i<=10;
+increment      = i++
+
+
+
+
+
+
+
+for (let i=1;i<=10;i++){
+    console.log("7*",i,"=",7*i);
+}
+
+
+for (let i=0;i<=10;i++){
+    console.log("3*",i,"=",3*i);
+}
+
+let a=24
+let fact=[]
+for(let i=1;i<=a;i++){
+    if (a %i==0){
+        fact.push(i)
+    }  
+}
+console.log(fact);
+
+
+
+
