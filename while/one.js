ec5ed52fd5f55404d7694c41fe70b8978c73bdf0
Kevikajyothi <jyothikevika2@gmail.com>
2023-05-18 18:29:47 +0530

    java

---
+++ while/one.js
@@ -0,0 +1,37 @@
+let array=[10,20,30,40,50]
+// console.log(array);
+// console.log(array[0]);
+// console.log(array[1]);
+// console.log(array[2]);
+// console.log(array[3]);
+// console.log(array[4]);
+
+
+
+// for(i=0 ; i<=4; i++){
+//     console.log(array[i]);
+// }
+
+// let i=0;
+// while(i<=4){
+//     console.log(array[i])
+//     i++;
+// }
+
+let i=0;
+do{
+     console.log(array[i]);
+     i++;
+}
+while (i<=4)
+
+
+let h=0;
+while (i<=4){
+    console.log(array[i])
+    i++;
+}
+
+for (i=0;i<=4;i++){
+console.log(array[i]);
+}
---
+++ while/three.js
@@ -0,0 +1,7 @@
+let array=[10,20,30,40,50]
+console.log(array);
+console.log(array[0]);
+console.log(array[1]);
+console.log(array[2]);
+console.log(array[3]);
+console.log(array[4]);
