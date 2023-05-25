f62be42c003ba052c5570c63521ad4cb6bb38ed2
Kevikajyothi <jyothikevika2@gmail.com>
2023-05-25 12:16:30 +0530

    okie

---
+++ while/js/fatarrow/color.js
@@ -0,0 +1,15 @@
+function cc_MouseOver(){
+    document.getElementById('mover').style.backgroundColor="yellow"
+}
+
+function cc_MouseOut(){
+    document.getElementById('mout').style.backgroundColor="pink"
+}
+
+let cc_Click=()=>{
+    document.getElementById('click').style.backgroundColor="red"
+}
+
+function cc_Focus(){
+    document.getElementById('focusid').style.backgroundColor="blue"
+}
