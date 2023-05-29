faff2d059f55547d22e0290263f95fa7d5e4573a
Kevikajyothi <jyothikevika2@gmail.com>
2023-05-29 14:39:08 +0530

    change image

---
+++ while/js/fatarrow/current location/one.js
@@ -0,0 +1,12 @@
+function changeImage(x,image)
+{
+if(x==1)
+{
+    image.src='https://images.pexels.com/photos/9932588/pexels-photo-9932588.jpeg?auto=compress&cs=tinysrgb&w=600';
+
+}
+if(x==2)
+{
+    image.src='https://images.pexels.com/photos/1646767/pexels-photo-1646767.jpeg?auto=compress&cs=tinysrgb&w=600';
+}
+}
