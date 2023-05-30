593e56f3bdc44913ad44f17337b344eda5cf3f43
Kevikajyothi <jyothikevika2@gmail.com>
2023-05-30 14:34:31 +0530

    bank account

---
+++ while/js/fatarrow/current location/zoom image/oop/one.js
@@ -0,0 +1,19 @@
+class Account{
+min_bal=500
+
+open_Account(){
+    console.log("open account succesfully");
+}
+diposite_Account(){
+    console.log("account open succesfully");
+}
+get_balance(){
+    console.log("check the balance");
+}
+get_statement(){
+    console.log("server busy");
+}
+}
+
+let a1 = new Account()
+a1.open_Account
